7cde7e563fd1c5bc881a6dd39d37ecf9a1ea4704
Vineet Karwa <vineetkarwa123@gmail.com>
2023-10-26 14:58:26 +0530

    Optimized code by reusing one function for different routes

--- WebsiteScrapper/DataRouter.js
+++ WebsiteScrapper/DataRouter.js
@@ -2,44 +2,49 @@ import request from 'request'
 import express from "express";
 import cheerio from "cheerio"
 
-const Fruits = express.Router();
-let url = 'https://www.commodityinsightsx.com/commodities/fruits'
+const dataItem = express.Router();
 
-let handleHtml = (html) => {
-    let Fruits = []
+let handleHtml = (html, category) => {
+    let dataItem = []
     let $ = cheerio.load(html)
     let itemArr = $(".boxContent")
-    let Fruit_name = $('.boxContent>h4')
+    let Item_name = $('.boxContent>h4')
     let images = $('.lozad')
     let table_of_content_value = $(".rc")
     for (let i = 0; i < itemArr.length; i++) {
         let data_of_each_item = {}
-        data_of_each_item["name"] = Fruit_name[i].children[0].data
+        data_of_each_item["name"] = Item_name[i].children[0].data
         data_of_each_item["image"] = images[i].attributes[1].value.includes("default")
             ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5_8U6-f_tDSw6t2z6AlyNfrDxSAQE8cPRMQ&usqp=CAU"
             : "https://www.commodityinsightsx.com" + images[i].attributes[1].value
-        data_of_each_item["Category"] = "Fruits"
+        data_of_each_item["Category"] = category
         data_of_each_item["Costliest_Market"] = table_of_content_value[7 * i + 2].children[0].data.trim()
         data_of_each_item["Costliest_Market_Price"] = table_of_content_value[7 * i + 3].children[0].data.trim()
         data_of_each_item["Cheapest_Market"] = table_of_content_value[7 * i + 4].children[0].data.trim()
         data_of_each_item["Cheapest_Market_Price"] = table_of_content_value[7 * i + 5].children[0].data.trim()
         data_of_each_item["Latest_Price_Date"] = table_of_content_value[7 * i + 6].children[0].data.trim()
-        Fruits.push(data_of_each_item)
+        dataItem.push(data_of_each_item)
     }
-    return Fruits
+    return dataItem
 }
 
-Fruits.get('/', async (req, res) => {
+dataItem.get('/:category', async (req, res) => {
+    const { category } = req.params;
+    const validCategories = ['pulses', 'spices', 'oilseeds', 'cereals', 'fruits'];
+    if (!validCategories.includes(category)) {
+        return res.status(404).send('Category not found');
+    }
+    const url = `https://www.commodityinsightsx.com/commodities/${category}`;
     request(url, (error, response, html) => {
         if (error) {
             console.error('error :', error);
         }
         else {
-            let data_of_Fruits = handleHtml(html)
-            res.send(data_of_Fruits)
+            let data_of_OilSeeds = handleHtml(html, category)
+            res.send(data_of_OilSeeds)
         }
     })
 })
 
 
-export default Fruits
+export default dataItem
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import Cereals from './WebsiteScrapper/Cereals.js'
-import Fruits from './WebsiteScrapper/Fruits.js'
-import OilSeeds from './WebsiteScrapper/OilSeeds.js'
-import pulses from './WebsiteScrapper/Pulses.js'
-import spices from './WebsiteScrapper/Spices.js'
 import cors from 'cors'
+import dataItem from './WebsiteScrapper/DataRouter.js'
 
 
 const app = express()
@@ -17,11 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/pulses', pulses)
-app.use('/spices', spices)
-app.use('/oilseeds', OilSeeds)
-app.use('/cereals', Cereals)
-app.use('/fruits', Fruits)
+app.use('/', dataItem)
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message })
